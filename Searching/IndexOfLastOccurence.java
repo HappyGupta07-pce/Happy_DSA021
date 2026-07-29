485e790ab5d581d39343250f173b9e9bbf555521
Happy Gupta <happygupta2019@gmail.com>
2026-07-29 22:21:56 +0530

    Add binary search for last occurrence index
    Introduce `IndexOfLastOccurence.java` in the Searching module. The program reads a sorted array and a target value, then uses binary search to find the target’s last index efficiently, printing either the index or a not-found message.

---
+++ Searching/IndexOfLastOccurence.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class IndexOfLastOccurence {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of array :");
+        int n = scan.nextInt();
+        System.out.println("Enter the sorted array elements :");
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scan.nextInt();
+        }
+        System.out.println("Enter the target element :");
+        int target = scan.nextInt();
+        
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2 ;
+            if(arr[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(arr[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        if(ans != -1){
+            System.out.println("Last occurence is at index :" + ans);
+        }
+        else{
+            System.out.println("Element not found in array");
+        }
+        scan.close();
+    }
+}
