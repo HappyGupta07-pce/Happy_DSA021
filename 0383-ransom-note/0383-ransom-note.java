6afe03e9fcf624c2bfdb6f54bbcebfb53e5fe4a2
Happy Gupta <220615759+HappyGupta07-pce@users.noreply.github.com>
2026-07-30 22:25:06 +0530

    Time: 1 ms (99.98%), Space: 46.4 MB (68.05%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length())
+            return false;
+
         int count[] = new int[26];
 
         for (char c : magazine.toCharArray()) {
