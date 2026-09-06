7ea673ea8c779368086a00d895a58ebcef698114
Happy Gupta <happygupta2019@gmail.com>
2026-09-06 23:26:50 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 0 ){
-            return 0;
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int money : nums){
+            int curr = Math.max(prev1,prev2 + money);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        if(n == 1){
-            return nums[0];
-        }
-        int dp[] = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++){
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-        return dp[n - 1];
+        return prev1;
     }
 }
