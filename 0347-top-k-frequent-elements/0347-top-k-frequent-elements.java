ee7ec5b89adfb46315d7cde99c044cd1b95cc6b5
Happy Gupta <happygupta2019@gmail.com>
2026-09-13 15:36:45 +0530

    Time: 14 ms (69.95%), Space: 47.8 MB (20.95%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int num : nums){
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> minheap = new PriorityQueue<>((a,b) -> freq.get(a) - freq.get(b));
+
+        for(int num : freq.keySet()){
+        minheap.offer(num);
+
+        if(minheap.size() > k){
+            minheap.poll();
+        }
+        }
+        int result[] = new int[k];
+        for(int i = k - 1; i >= 0; i--){
+            result[i] = minheap.poll();
+        }
+        return result;
+    }
+}
